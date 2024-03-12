fc2abe8b007a9a158abf5d7972b6673ec4fea728
lgcastellucci <castellucci@gmail.com>
2024-03-11 21:57:46 -0300

    Subindo a primeira versão, ainda somente abrindo o arquivo do projeto e colocando ele em um XML

---
+++ LeitorCsproj.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+
+namespace VerificadorDuplicidadeCodigosRetorno
+{
+    public class LeitorCsproj
+    {
+        public List<string> LerItensCompile(string caminhoCsproj)
+        {
+            // Lista para armazenar os itens "Compile Include"
+            var itensCompile = new List<string>();
+
+            // Carregar o documento XML
+            var documentoXml = new XmlDocument();
+            documentoXml.Load(caminhoCsproj);
+
+            // Obter os nós "Compile"
+            var nosCompile = documentoXml.SelectNodes("Compile");
+
+            // Extrair o atributo "Include" de cada nó "Compile"
+            foreach (XmlNode noCompile in nosCompile)
+            {
+                string itemCompile = noCompile.Attributes["Include"].Value;
+                itensCompile.Add(itemCompile);
+            }
+
+            return itensCompile;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+using System.Xml;
+
+namespace VerificadorDuplicidadeCodigosRetorno
+{
+    class Program
+    {
+        static void Main()
+        {
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            Console.Title = "VerificadorDuplicidadeCodigosRetorno";
+
+            do
+            {
+                Console.Write(LineAsterisk());
+                Console.WriteLine("Analise de arquivos .csproj");
+                Console.Write(LineAsterisk());
+                Console.Write("Entre com o caminho do arquivo (ou 'q' para sair):");
+
+                var val = Console.ReadLine();
+
+                if (string.Equals(val, "q", StringComparison.OrdinalIgnoreCase))
+                    return;
+
+                if (!File.Exists(val))
+                {
+                    Console.WriteLine($"Arquivo não encontrado: {val}.");
+                    continue;
+                }
+
+                val = "C:\\Projetos\\aspx\\Servicos\\branches\\02.01\\Servicos\\Servicos.csproj";
+                if (File.Exists(val))
+                {
+                    if (Path.GetExtension(val) == ".csproj")
+                    {
+                        var leitorCsproj = new LeitorCsproj();
+                        var itensCompile = leitorCsproj.LerItensCompile(val);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"O arquivo {val} não é um arquivo conhecido.");
+                        continue;
+                    }
+
+                }
+
+            } while (true);
+
+        }
+        public static string LineAsterisk()
+        {
+            string result = string.Empty;
+            for (int i = 0; i < Console.WindowWidth; i++)
+                result += "*";
+
+            return result;
+        }
+
+        
+
+    }
+}
